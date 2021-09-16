27ce23bd01b909842cbbfacdc4f34ac3a9d2a460
Tobias Erdle <tobi.erdle@gmail.com>
2021-09-16 15:51:18 +0200

    Add switch-jdk.sh

---
+++ switch-jdk.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ "${1}" == '-h' ]]; then
+
+    echo "switch-jdk: Switches the current JDK located in /usr/lib/jvm/default"
+    echo "Attention: Has to be executed as root"
+    echo ""
+    echo "Usage: sh switch-jdk.sh [-h] [VERSION]"
+    echo ""
+    echo "-h: Shows this help"
+    echo "VERSION: The JDK version to use. Exists when no version is set."
+    
+    exit
+fi
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+java_version=${1}
+
+echo "Searchig for install dir in /usr/lib/jvm for version $java_version"
+installed_dir=$(find /usr/lib/jvm/ -name "java-$java_version-*" -type d)
+
+if [[ -z $installed_dir ]]; then
+	echo "Could not find install dir for JDK $java_version. Exit program..."
+	exit
+else
+	echo "Found install dir $installed_dir. Linking to /usr/lib/jvm/default"
+	rm /usr/lib/jvm/default
+	ln -s $installed_dir /usr/lib/jvm/default
+fi
