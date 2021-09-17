85e51d4787dcb0ed9c3b4c24ba9274538fec913f
Tobias Erdle <tobi.erdle@gmail.com>
2021-09-17 13:44:48 +0200

    Make scripts in /usr/bin executable

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ echo "Starting installation of fedora-java-scripts"
 
 echo "Copy install-jdk.sh into /usr/bin ignoring existing script"
 cp -r install-jdk.sh /usr/bin/install-jdk
+chmod +x /usr/bin/install-jdk
 
 echo "Copy switch-jdk.sh into /usr/bin ignoring existing script"
 cp -r switch-jdk.sh /usr/bin/switch-jdk
+chmod +x /usr/bin/switch-jdk
